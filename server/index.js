097ef59e6cc8a82a97691d5bc51774ddf200edf8
Tejas <tejas@retinus.io>
2021-07-24 19:56:20 -0700

    socket connection setup established

--- server/index.js
+++ server/index.js
@@ -6,7 +6,24 @@ const PORT = process.env.PORT || 5000;
 const router = require('./router');
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+corsOptions = {
+  cors: true,
+  origins: ['http://localhost:3000'],
+};
+
+const io = socketio(server, corsOptions);
+
+io.on('connection', (socket) => {
+  console.log('We have a new connection');
+
+  socket.on('join', ({ name, room }) => {
+    console.log(name, room);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user had left');
+  });
+});
 
 app.use(router);
 
