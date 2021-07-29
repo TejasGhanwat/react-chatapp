1470be041252d1115361e33bf2657f63f9ce8b25
Tejas <tejas@retinus.io>
2021-07-29 09:39:25 -0700

    created chat and input components

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import Infobar from '../InfoBar/InfoBar.js';
+import Input from '../Input/Input.js';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
@@ -21,11 +25,39 @@ const Chat = ({ location }) => {
 
     return () => {
       socket.emit('disconnect');
-
       socket.off();
     };
   }, [ENDPOINT, location]);
-  return <h1>Chat</h1>;
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => {
+        setMessage('');
+      });
+    }
+  };
+
+  console.log(message, messages);
+  return (
+    <div className='outerContainer'>
+      <div className='container'>
+        <Infobar room={room} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
